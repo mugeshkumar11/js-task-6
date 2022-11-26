3ad01f829b25aa9a497a91843309486fce174edd
mugeshkumar <kmugesh11199@gmail.com>
2022-11-26 19:46:54 +0530

    add new file

--- js/main.js
+++ js/main.js
@@ -1,19 +1,88 @@
-let text=document.getElementById("text");
+//text color change random
 
-let toggleBtn = document.getElementById("togglebtn");
+// let text=document.getElementById("text");
 
-let colors = ["yellow",'red','blue','green'];
+// let toggleBtn = document.getElementById("togglebtn");
 
-let flag = 0;
+// let result ="";
 
-var handleToggle = function () {
-    text.style.color= colors[flag];
-    flag++;
-    if (flag >= 3){
-       flag=0;
-    }
+// function randomColors() {
+    
+//     result = Math.ceil(Math.random()*260)
+//     text.style.color= "#" + result;
+//   }
+// toggleBtn.addEventListener('click', randomColors);
 
 
-};
 
-toggleBtn.addEventListener('click', handleToggle);
+//1 to 15 even number loop
+
+// var input = 15;
+
+// for(let i=0; i<=input; i++){
+//     if(i%2==0){
+//         console.log("even number ", i);
+//     }
+
+// }
+
+//1 to 100 cube number
+
+
+// var input = 4;
+// var sum = 0;
+
+// for (let i=0; i<=input; i++){
+//     sum += i**3;
+//     console.log(sum);
+// }
+
+
+//123 to 321 for three two one
+
+// var input = 123;
+
+// while(input> 0){
+
+// let rem = input%10
+// input = ((input-rem)/10)
+
+// if(rem==1){
+//  console.log("one");
+// }else if(rem==2){
+//  console.log("two");
+// }else if(rem==3){
+//  console.log("three");
+// }else if(rem==4){
+//  console.log("four");
+// }else if(rem==5){
+//  console.log("five");
+// }else if(rem==6){
+//  console.log("six");
+// }else if(rem==7){
+//  console.log("seven");
+// }else if(rem==8){
+//  console.log("eght");
+// }else if(rem==9){
+//  console.log("nine");
+// }else
+// console.log("zero");
+
+
+// };
+
+//array for vowels method
+
+var input2 = ["mugeshkumar"];
+var count =0;
+for(let i=0; i<input2.length; i++){
+  if(input2[i]=="a" ||input2[i]=="e" ||input2[i]=="i" ||input2[i]=="o" ||input2[i]=="u" ){
+             count ++;
+  }
+ 
+}
+console.log(count);
+
+
+
+
